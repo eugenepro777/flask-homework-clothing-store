93dcf64dae6d672286459aff0a0df9acf642c69c
Eugene_Pro <proskuryakov.e@list.ru>
2024-01-14 12:47:15 +0300

    update form.py, register.html

--- registration_form_with_db/form.py
+++ registration_form_with_db/form.py
@@ -1,17 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from wtforms.validators import DataRequired, Email, Length, ValidationError
 from registration_form_with_db.model import User
 
 
 # добавлена проверка наличия/отсутствия пользователя с одинаковым email в нашей базе данных
-# поля с подтверждением пароля по условиям задачи нет, поэтому поле для сравнения мы добавлять не будем
+# поля с подтверждением пароля по условиям задачи нет, поэтому поле для сравнения пароля мы добавлять не будем
 class RegistrationForm(FlaskForm):
     first_name = StringField('Имя', validators=[DataRequired()])
     last_name = StringField('Фамилия', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
-    # password_confirm = PasswordField('Подтверждение пароля', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Отправка данных')
 
     def validate_email(self, field):
