464b8267a9999b3ec2bdb5dc9e5e00254083a6ae
Eugene_Pro <proskuryakov.e@list.ru>
2024-01-14 12:49:27 +0300

    database path has been updated

--- registration_form_with_db/app_registration_db.py
+++ registration_form_with_db/app_registration_db.py
@@ -7,7 +7,7 @@ from registration_form_with_db.model import db, User
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'7bb2c87f65986c3843ea8c6d1750beee37bb0925e362df20034dc8757f1aae28'
 csrf = CSRFProtect(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///registration_form_with_db/instance/users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../instance/users.db'
 db.init_app(app)
 
 
@@ -21,7 +21,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(first_name=form.first_name.data,
-                    last_name=form.last_name,
+                    last_name=form.last_name.data,
                     email=form.email.data)
         # установку пароля делаем через отдельный метод класса User
         user.set_password(form.password.data)
