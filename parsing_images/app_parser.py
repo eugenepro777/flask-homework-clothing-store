8f6941c3b4f7de0cec99f7694440cee24e20214f
Eugene_Pro <proskuryakov.e@list.ru>
2024-01-21 13:16:56 +0300

    a function has been added in which time is compared

--- parsing_images/app_parser.py
+++ parsing_images/app_parser.py
@@ -31,5 +31,57 @@ async def download_image_async(url):
                     f.write(await response.read())
 
 
+# создана, чтобы обойти warning deprecation при работе через loop, для второго варианта обработки через asyncio.run
 async def download_images_asynchronous(urls):
     await asyncio.gather(*(download_image_async(url) for url in urls))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Скачивание с заданных URL-адресов')
+    parser.add_argument('urls', nargs='+', help='')
+    args = parser.parse_args()
+
+    start_time = time.time()
+    for url in args.urls:
+        download_image_sync(url)
+    sync_time = time.time() - start_time
+    print(f"Синхронный подход, сохранение за: {sync_time:.4f} секунд")
+
+    threads = []
+    start_time = time.time()
+    for url in args.urls:
+        thread = threading.Thread(target=download_image_sync, args=(url,))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+    multithread_time = time.time() - start_time
+    print(f"Мультипоточный подход, сохранение за: {multithread_time:.4f} секунд")
+
+    processes = []
+    start_time = time.time()
+    for url in args.urls:
+        process = multiprocessing.Process(target=download_image_sync, args=(url,))
+        processes.append(process)
+        process.start()
+    for process in processes:
+        process.join()
+    multiprocess_time = time.time() - start_time
+    print(f"Мультипроцессорный подход, сохранение за: {multiprocess_time:.4f} секунд")
+
+    tasks = []
+    start_time = time.time()
+    # первый вариант, работаем через loop, у меня вызывает warning deprecation
+    for url in args.urls:
+        task = asyncio.ensure_future(download_image_async(url))
+        tasks.append(task)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(tasks))
+    # второй вариант, warning deprecation не вызывает, отрабатывает без проблем
+    # asyncio.run(download_images_asynchronous(args.urls))
+    asynchronous_time = time.time() - start_time
+    print(f"Асинхронный подход, сохранение за: {asynchronous_time:.4f} секунд")
+
+
+if __name__ == '__main__':
+    main()
