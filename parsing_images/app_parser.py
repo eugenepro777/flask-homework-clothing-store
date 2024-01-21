e83a084caa0f72aaafea6cd90a12c2b4db0947ff
Eugene_Pro <proskuryakov.e@list.ru>
2024-01-21 12:43:44 +0300

    asynchronous functions have been added

--- parsing_images/app_parser.py
+++ parsing_images/app_parser.py
@@ -18,3 +18,18 @@ def download_image_sync(url):
         save_path = os.path.join(IMAGE_FOLDER, os.path.basename(url))
         with open(save_path, "wb") as f:
             f.write(response.content)
+
+
+async def download_image_async(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if not os.path.exists(IMAGE_FOLDER):
+                os.mkdir(IMAGE_FOLDER)
+            if response.status == 200:
+                save_path = os.path.join(IMAGE_FOLDER, os.path.basename(url))
+                with open(save_path, "wb") as f:
+                    f.write(await response.read())
+
+
+async def download_images_asynchronous(urls):
+    await asyncio.gather(*(download_image_async(url) for url in urls))
