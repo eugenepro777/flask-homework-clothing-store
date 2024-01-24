67878bec544dcbd0defd79a4462cb609382b9fb5
Eugene_Pro <proskuryakov.e@list.ru>
2024-01-24 14:02:41 +0300

    the code was updated after testing the application

--- tasks_tracker/app_tasks_tracker.py
+++ tasks_tracker/app_tasks_tracker.py
@@ -19,15 +19,13 @@ class Task(BaseModel):
 
 @app.get("/tasks", response_class=HTMLResponse)
 async def read_tasks(request: Request):
-    tasks_table = pd.DataFrame([vars(task) for task in tasks]).to_html()
+    tasks_table = pd.DataFrame([vars(task) for task in tasks]).to_html(index=False)
     return templates.TemplateResponse("tasks.html", {"request": request, "tasks_table": tasks_table})
 
 
 @app.get("/tasks/{task_id}", response_class=HTMLResponse)
 async def read_task(request: Request, task_id: int):
-    if task_id >= len(tasks):
-        raise HTTPException(status_code=404, detail="Задача не найдена")
-    task = pd.DataFrame([vars(t) for t in tasks if t.id == task_id]).to_html()
+    task = pd.DataFrame([vars(t) for t in tasks if t.id == task_id]).to_html(index=False)
     return templates.TemplateResponse("tasks.html", {"request": request, "task": task})
 
 
@@ -41,7 +39,7 @@ async def create_task(task: Task):
 
 @app.put("/tasks/{task_id}", response_model=Task)
 async def update_task(task_id: int, task: Task):
-    if task_id >= len(tasks):
+    if task_id <= 0 or task_id > len(tasks):
         raise HTTPException(status_code=404, detail="Задача не найдена")
     for i, new_task in enumerate(tasks):
         if new_task.id == task_id:
@@ -52,7 +50,7 @@ async def update_task(task_id: int, task: Task):
 
 @app.delete("/tasks/{task_id}", response_model=Task)
 async def delete_task(task_id: int):
-    if task_id >= len(tasks):
+    if task_id <= 0 or task_id > len(tasks):
         raise HTTPException(status_code=404, detail="Задача не найдена")
     for i, remove_task in enumerate(tasks):
         if remove_task.id == task_id:
